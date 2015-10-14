765cd97c99260102a19bf2039426d7cd31b653b8
Joey <me@joeygaskin.com>
2015-10-13 23:31:02 -0400

    Update 2015-10-14T03:30:23.519Z

--- .tmp/scripts/templates.js
+++
@@ -1,4 +0,0 @@
-this["JST"] = this["JST"] || {};
-this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    return "<ul class=\"likes\"><button id=\"addbutton\">likes</button></ul>\n";
-},"useData":true});
